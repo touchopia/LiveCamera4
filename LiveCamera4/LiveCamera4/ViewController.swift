a41698f6c48e773ce32df4ba710b572082f552f7
Phil Wright <touchopia@me.com>
2017-08-28 10:55:46 -0600

    Adding XCode 8 Project

---
+++ LiveCamera4/LiveCamera4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveCamera4
+//
+//  Created by Phil Wright on 8/28/17.
+//  Copyright © 2017 Touchopia, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
